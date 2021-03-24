f3fc4f77390e8534da5ba761aabc522376107aaa
kiruha2002 <71868436+kiruha2002@users.noreply.github.com>
2021-03-24 22:44:10 +0300

    Add files via upload

---
+++ lab1/4st.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int lenght_rect;
+
+    cin >> lenght_rect;
+    for (int i = 0; i <= lenght_rect; i++){
+        for(int j = 0; j <= lenght_rect - i; j++){
+    cout << '*';
+    }
+    cout << endl; }
+    return 0;
+}
