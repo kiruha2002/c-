02a8c38ce362765ce2cff72525bda76bd0cb64ed
kiruha2002 <71868436+kiruha2002@users.noreply.github.com>
2021-03-26 01:36:01 +0300

    Add files via upload

---
+++ lab5/createemptystack.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<int> mystack;
+    mystack.push(1);
+
+    // Stack becomes 1
+
+    if (mystack.empty()) {
+        cout << "True";
+    }
+    else {
+        cout << "False";
+    }
+    return 0;
+}
