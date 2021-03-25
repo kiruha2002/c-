d3f6f8f10cf82362197dc452678e2681c3a17d06
kiruha2002 <71868436+kiruha2002@users.noreply.github.com>
2021-03-26 00:44:59 +0300

    Add files via upload

---
+++ lab2/7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+
+int main() {
+  int a;
+  int b = 1;
+  int i = 2;
+  int c;
+  cin >> a;
+  c= floor(sqrt(a));
+  while (i <= c)
+    {
+    if (a % i == 0)
+    {
+      b = 0;
+      break;
+     }
+  i++;
+  }
+
+cout << b;
+
+}
+© 2021 GitHub, Inc.
